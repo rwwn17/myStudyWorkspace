db5973b0bfdec026e058c7931cb87fb634d982d5
rwwn17 <roger_wuthrich@hotmail.com>
2024-09-03 19:35:39 -0300

    simplesCreateClassProdutoTest.java
    Just a test class that creates an object called as Product (in Portuguese "Produto").

---
+++ entities/Produto.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Produto{
+
+    String name;
+
+    public Produto(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    
+}
